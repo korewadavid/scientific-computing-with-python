e7ad52cd85c2f6f18508d7b19b9646cb1c586a16
korewadavid <korewadavid@gmail.com>
2023-10-16 13:57:45 +0200

    added arthmetic-formatter

--- python-tutorial/python-tutorial.py
+++ python-tutorial/python-tutorial.py
@@ -1,4 +1,21 @@
 # Data Types (int, float, string '' or "", bool: true or false)
 
 # Output and Printing
-print("hello world -.-", 'end', 4.5)
+#print("hello world -.-", 'end', 4.5)
+
+# Variables
+
+# Input
+name = input("Name: ")
+age = input("Age: ")
+print("Hello", name, "you are", age, "years old")
+
+# Operators
+# / returns a float, int(x / y) to make it return an int
+
+# String methods
+hello = "hello"
+print(hello.upper()) 
+
+hello = "heLLO World"
+print(hello.lower().count("ll"))
